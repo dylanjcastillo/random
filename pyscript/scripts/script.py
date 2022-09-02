ab86558475505738a5c97d50641e4f49fce6ac9a
Dylan Castillo <dylanjcastillo@gmail.com>
2022-09-02 13:34:48 +0200

    Delete pandas cheatsheet

--- pyscript/scripts/script.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-from pyodide.http import open_url
-
-# Create sample dataframe
-url = "https://raw.githubusercontent.com/dylanjcastillo/random/main/pyscript/data/iris_modified.csv"
-df = pd.read_csv(open_url(url))
-df_initial = df.copy()
